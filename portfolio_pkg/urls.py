322335e8668b447fd40965490145d896e4a0befc
Gilberto Junior <129233842+jotaTorrezDev@users.noreply.github.com>
2026-09-20 02:15:38 -0300

    Use environment variable for admin URL

--- portfolio_pkg/urls.py
+++ portfolio_pkg/urls.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from decouple import config
+
+ADMIN_URL = config('ADMIN_URL', default='admin/')
+
 urlpatterns = [
-    path('painel-gj-x7k2/', admin.site.urls),
+    path(ADMIN_URL, admin.site.urls),
     path('', include('core.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
