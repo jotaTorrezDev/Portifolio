b117c88d3855a3652100311b60280b2d671e0716
Gilberto Junior <129233842+jotaTorrezDev@users.noreply.github.com>
2026-09-20 02:06:59 -0300

    Fix URL path syntax in urls.py

--- portfolio_pkg/urls.py
+++ portfolio_pkg/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    path('painel-gj-x7k2/' admin.site.urls),
+    path('painel-gj-x7k2/', admin.site.urls),
     path('', include('core.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
