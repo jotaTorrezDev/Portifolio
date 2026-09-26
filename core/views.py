c0d24745da02023f762f3e7ac55cd5ba0d80a9b0
Gilberto Junior <129233842+jotaTorrezDev@users.noreply.github.com>
2026-09-26 04:09:53 -0300

    Fix syntax error in render function call

--- core/views.py
+++ core/views.py
@@ -61,4 +61,4 @@ def contato(request):
             else:
                 messages.success(request,'Mensagem enviada! Responderei em breve.')
                 return redirect('contato')
-    return render (request, 'contato.html', {'perfil' perfil})
+        return render (request, 'contato.html', {'perfil' perfil})
